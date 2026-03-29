f558f07e733ba68c82b0ff29d9f71dcf6950df1f
Aryan Ranjan <ranjanaryan99@gmail.com>
2026-03-29 15:25:37 +0530

    Implemented GET subject route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.ts";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
+
+app.use('/api/subjects',subjectsRouter)
 
 app.get('/',(req,res) => {
     res.send("Welcome to classroom api");
